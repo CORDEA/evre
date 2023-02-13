9f7698aa2a830c84ccc6a14032b7c2331b14d13a
Yoshihiro Tanaka <contact@cordea.jp>
2023-02-13 13:48:48 +0900

    reformat code

--- evre/Platforms/Android/MainActivity.cs
+++ evre/Platforms/Android/MainActivity.cs
@@ -4,8 +4,9 @@ using Android.OS;
 
 namespace evre;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
-
--- evre/Platforms/iOS/AppDelegate.cs
+++ evre/Platforms/iOS/AppDelegate.cs
@@ -5,6 +5,8 @@ namespace evre;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        return MauiProgram.CreateMauiApp();
+    }
 }
-
