9b4172118beca8b2b7f163885419e741fff51691
Yoshihiro Tanaka <contact@cordea.jp>
2023-02-15 12:36:12 +0900

    define a custom uri scheme

--- evre/Platforms/Android/MainActivity.cs
+++ evre/Platforms/Android/MainActivity.cs
@@ -1,12 +1,20 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 
 namespace evre;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+[Activity(
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
-                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density
+)]
+[IntentFilter(new[] { Intent.ActionView },
+    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+    DataScheme = "jp.cordea.evre"
+)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
